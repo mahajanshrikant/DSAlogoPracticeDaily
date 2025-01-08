5b024d248f5cdffd0959b477eeac3b8060f875fb
shrikant mahajan <shrikantmahajan10@gmail.com>
2025-01-08 21:00:05 +0530

    added sorting logic

---
+++ GoalToExpert/src/com/expert/ArraysSortingBasic.java
@@ -0,0 +1,29 @@
+package com.expert;
+
+public class ArraysSortingBasic {
+
+	public static void main(String[] args) {
+		
+		
+		int arr[]= {4,3,2,1};
+		
+		for(int i=0;i<arr.length;i++) {
+			
+			// Inner nested loop pointing 1 index ahead
+			for(int j=i+1;j<arr.length;j++) {
+				
+				 // Checking elements
+                int temp = 0;
+                if (arr[j] < arr[i]) {
+
+                    // Swapping
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+			}
+			  System.out.print(arr[i] + " ");
+		}
+		
+	}
+}
---
+++ GoalToExpert/src/com/expert/ExpertPractice.java
@@ -0,0 +1,10 @@
+package com.expert;
+
+public class ExpertPractice {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
